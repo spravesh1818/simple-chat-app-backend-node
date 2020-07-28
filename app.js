dbbb72c93ac15d40358bed04d3dd740cf87c0a0d
Pravesh Chapagain <spravesh1818@gmail.com>
2020-07-28 18:04:48 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express=require('express');
+var app=express();
+
+
+app.get('/',function(req,res){
+    res.send("Hello World");
+})
+
+server=app.listen(3000,function(){
+    console.log("App listening at port 3000");
+})
+
+
+
+//socket io
+
+const io=require('socket.io')(server);
+const users=[];
+const connections=[];
+
+
+io.on('connection',(socket)=>{
+    console.log("User connected");
+
+    socket.on('join',(data)=>{
+        console.log(data)
+    })
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected");
+    })
+
+})
+
